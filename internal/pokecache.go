765bf06c4c7de9f6bd1ce2f906bcc2985f5fc0e9
Jared Key <key.jared.a@gmail.com>
2025-03-09 12:09:33 +0100

    stuff

--- internal/pokecache.go
+++ internal/pokecache.go
@@ -2,9 +2,35 @@ package pokecache
 
 import "time"
 
-type Cache map[string]cacheEntry
+type Cache struct {
+	entries  map[string]cacheEntry
+	interval time.Duration
+}
 
 type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
+}
+
+func NewCache(interval time.Duration) Cache {
+	cache := Cache{
+		entries:  make(map[string]cacheEntry),
+		interval: interval,
+	}
+	return cache
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.entries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c Cache) Get(key string) ([]byte, bool) {
+	entry, ok := c.entries[key]
+	if !ok {
+		return []byte{}, false
+	}
+	return entry.val, true
 }
