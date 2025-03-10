19392129b270822bf78b40334d3822f75f842d6a
Jared Key <key.jared.a@gmail.com>
2025-03-10 20:23:12 +0100

    organize, and catch!

---
+++ commands.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func([]string) error
+}
+
+var commands map[string]cliCommand
+
+func initCommands() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Display 20 locations",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays previous 20 locations",
+			callback:    commanMapb,
+		},
+		"explore": {
+			name:        "explore",
+			description: "Show encounters in a specified area",
+			callback:    commandExplore,
+		},
+		"catch": {
+			name:        "catch",
+			description: "Catch a pokemon",
+			callback:    commandCatch,
+		},
+	}
+}
+
+func commandExit(args []string) error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return fmt.Errorf("error exiting")
+}
+
+func commandHelp(args []string) error {
+	listOfCommands := ""
+	for key, value := range commands {
+		listOfCommands += fmt.Sprintf("%s: %s\n", key, value.description)
+	}
+
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n%s", listOfCommands)
+	return nil
+}
+
+func commandExplore(args []string) error {
+	if len(args) < 2 {
+		return errors.New("missing location area name")
+	}
+	// fmt.Println(args[1])
+	locationAreaName := args[1]
+
+	content, err := getRequestWithName(locationAreaName)
+	if err != nil {
+		return err
+	}
+
+	err = printPokemon(content)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func commandMap(args []string) error {
+	body, err := getRequest()
+	if err != nil {
+		return err
+	}
+
+	err = printMaps(body)
+	if err != nil {
+		return err
+	}
+
+	offset += 20
+	return nil
+}
+
+func commanMapb(args []string) error {
+	offset -= 20
+	if offset < 0 {
+		offset = 0
+	}
+
+	body, err := getRequest()
+	if err != nil {
+		return err
+	}
+
+	err = printMaps(body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func commandCatch(args []string) error {
+	if len(args) < 2 {
+		return errors.New("missing pokemon name")
+	}
+
+	pokemonName := args[1]
+
+	fmt.Printf("Throwing a Pokeball at %v...\n", pokemonName)
+	content, err := getPokemonRequest(pokemonName)
+	if err != nil {
+		return err
+	}
+
+	var pokemon Pokemon
+	err = json.Unmarshal(content, &pokemon)
+	if err != nil {
+		return err
+	}
+
+	baseXP := pokemon.BaseExperience
+	random := rand.Intn(baseXP)
+	// fmt.Printf("%v's base xp: %v\n", pokemonName, baseXP)
+
+	if random < 20 {
+		fmt.Printf("%v was caught!\n", pokemonName)
+	} else {
+		fmt.Printf("%v escaped!\n", pokemonName)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -14,48 +13,11 @@ import (
 	"github.com/mrjkey/pokedexcli/internal/pokecache"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func([]string) error
-}
-
-var commands map[string]cliCommand
-
-func initCommands() {
-	commands = map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the pokedex",
-			callback:    commandExit,
-		},
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-		"map": {
-			name:        "map",
-			description: "Display 20 locations",
-			callback:    commandMap,
-		},
-		"mapb": {
-			name:        "mapb",
-			description: "Displays previous 20 locations",
-			callback:    commanMapb,
-		},
-		"explore": {
-			name:        "explore",
-			description: "Show encounters in a specified area",
-			callback:    commandExplore,
-		},
-	}
-}
-
 var cache pokecache.Cache
 
 func main() {
 	initCommands()
+	// rand.Seed(time.Now().UnixNano())
 	cache = pokecache.NewCache(time.Second * 5)
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -75,22 +37,6 @@ func main() {
 	}
 }
 
-func commandExit(args []string) error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return fmt.Errorf("error exiting")
-}
-
-func commandHelp(args []string) error {
-	listOfCommands := ""
-	for key, value := range commands {
-		listOfCommands += fmt.Sprintf("%s: %s\n", key, value.description)
-	}
-
-	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n%s", listOfCommands)
-	return nil
-}
-
 // 2100 curl https://pokeapi.co/api/v2/location/
 // 2101 curl https://pokeapi.co/api/v2/location/?offset=0&limit=20
 // 2102 curl https://pokeapi.co/api/v2/location/?offset=20&limit=20
@@ -103,6 +49,11 @@ func getRequest() ([]byte, error) {
 	limitStr := fmt.Sprintf("limit=%v", limit)
 	url := fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?%v&%v", offsetStr, limitStr)
 
+	return processRequest(url)
+}
+
+func getPokemonRequest(pokemonName string) ([]byte, error) {
+	url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%v", pokemonName)
 	return processRequest(url)
 }
 
@@ -154,8 +105,9 @@ type Encounter struct {
 }
 
 type Pokemon struct {
-	Name string `json:"name"`
-	Url  string `json:"url"`
+	Name           string `json:"name"`
+	BaseExperience int    `json:"base_experience"`
+	Url            string `json:"url"`
 }
 
 func printMaps(body []byte) error {
@@ -181,60 +133,6 @@ func printPokemon(content []byte) error {
 	for _, result := range encResp.Encounters {
 		fmt.Println(result.Pokemon.Name)
 	}
-	return nil
-}
-
-func commandExplore(args []string) error {
-	if len(args) < 2 {
-		return errors.New("missing location area name")
-	}
-	// fmt.Println(args[1])
-	locationAreaName := args[1]
-
-	content, err := getRequestWithName(locationAreaName)
-	if err != nil {
-		return err
-	}
-
-	err = printPokemon(content)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func commandMap(args []string) error {
-	body, err := getRequest()
-	if err != nil {
-		return err
-	}
-
-	err = printMaps(body)
-	if err != nil {
-		return err
-	}
-
-	offset += 20
-	return nil
-}
-
-func commanMapb(args []string) error {
-	offset -= 20
-	if offset < 0 {
-		offset = 0
-	}
-
-	body, err := getRequest()
-	if err != nil {
-		return err
-	}
-
-	err = printMaps(body)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
